fca1f08c933c7cb2754f03a36d82305c5f9d74c2
Girts <girts.strazdins@gmail.com>
2019-12-12 22:44:09 +0100

    Add animations
    Fix bug with gravity
    Change speeds to look realistic with the animations

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY_ACCEL = 9.8
-const MOTION_SPEED = 100
+const MOTION_SPEED = 160
 const JUMP_ACCEL = 320
 
 var motion = Vector2()
@@ -11,18 +11,25 @@ func _physics_process(delta):
 	# Set motion vector to left/right by keys
 	if Input.is_action_pressed("ui_right"):
 		motion.x = MOTION_SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -MOTION_SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
 		motion.x = 0
+		$Sprite.play("Idle")
 		
 	
 	# Apply gravity: 9.8m/s^2 every second when we are not touching a floor
+	motion.y += GRAVITY_ACCEL
+
 	if is_on_floor():
 		# Allow to jump when on the floor
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y -= JUMP_ACCEL
 	else:
-		motion.y += GRAVITY_ACCEL
+		$Sprite.play("Jump")
 		
 	motion = move_and_slide(motion, UP)
