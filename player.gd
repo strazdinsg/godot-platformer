f66a97e91a2208a4dcdc27956ec4a440a3ae4e94
Girts <girts.strazdins@gmail.com>
2019-12-12 22:10:07 +0100

    Use proper constants for motion

--- player.gd
+++ player.gd
@@ -1,14 +1,18 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
+const GRAVITY_ACCEL = 9.8
+const MOTION_SPEED = 100
+const JUMP_ACCEL = 320
+
 var motion = Vector2()
 
 func _physics_process(delta):
 	# Set motion vector to left/right by keys
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = MOTION_SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -MOTION_SPEED
 	else:
 		motion.x = 0
 		
@@ -17,8 +21,8 @@ func _physics_process(delta):
 	if is_on_floor():
 		# Allow to jump when on the floor
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y -= 320
+			motion.y -= JUMP_ACCEL
 	else:
-		motion.y += 9.8
+		motion.y += GRAVITY_ACCEL
 		
 	motion = move_and_slide(motion, UP)
