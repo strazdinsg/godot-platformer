2ab714ca613fc6605080625d34f4b69710d8ef9e
Girts <girts.strazdins@gmail.com>
2019-12-12 21:25:13 +0100

    Add gravity to the player

--- player.gd
+++ player.gd
@@ -3,10 +3,15 @@ extends KinematicBody2D
 var motion = Vector2()
 
 func _physics_process(delta):
+	# Set motion vector to left/right by keys
 	if Input.is_action_pressed("ui_right"):
 		motion.x = 100
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -100
 	else:
 		motion.x = 0
+		
+	# Apply gravity: 9.8m/s^2 every second
+	motion.y += 9.8
+		
 	move_and_slide(motion)
