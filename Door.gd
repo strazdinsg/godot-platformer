cb7a1363b9edce2f1115398674f0245a9c4b81b3
Girts <girts.strazdins@gmail.com>
2019-12-15 23:05:12 +0100

    Extract player and door as separate scenes, instantiate them
    Export nextScene as a variable

--- Door.gd
+++ Door.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
+export (String, FILE, "*.tscn") var nextScene;
+
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "player":
 			print("Reached door!")
-			get_tree().change_scene("World2.tscn")
+			if nextScene:
+				get_tree().change_scene(nextScene)
